c69202f2b353b7204373dd974a70330834776dbd
soham khot <sohamkhot75@gmail.com>
2025-06-22 20:04:17 +0530

    🐛 fix every bugs

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+dotenv.config();
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
@@ -6,7 +7,7 @@ const path = require("path");
 const connectDB = require("./config/db");
 
 // Load environment variables
-dotenv.config();
+
 
 const app = express();
 
